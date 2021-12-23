377bca9202115be9bb0fd34790700f9e6a8deb1f
vladimir360 <vladimir360@mail.ru>
2021-12-23 21:34:02 +0300

    first working application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Список задач</h1>
+    <div v-if="count() == 0">Вы великолепны!</div>
+    <div v-else-if="count() == 1">Осталя последний рывок!</div>
+    <div v-else>Осталось сделать задач: <span class="counter">{{count()}}</span></div>
+    <div class="list">
+      <div class="item" v-bind:class="{done: task.done}" v-for='task in tasks' :key="task">
+        <input type="checkbox" v-model="task.done">
+        {{task.text}}
+      </div>
+    </div>
+    <div class="form">
+      <input v-model="textTask">
+      <input type="submit" value="Добавить" @click="addTask">
+    </div>
+    <transition name="bounce">
+      <img src="rob.jpg" alt="#" v-show="count() == 0">
+    </transition>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+        return {
+            nameForTask: 'Название задачи',
+            tasks: [
+                {text: 'Развернуть окружение в Codepen', done: true},
+                {text: 'Пройти курс по Vue', done: false},
+                {text: 'Сделать интернет-магазин на Vue', done: false}
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push({text: this.textTask, done: false});
+            this.textTask = '';
+        },
+        count() {
+            return this.tasks.filter(task => !task.done).length;
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { application } from './application'
 
 
 
-//createApp(App).mount('#app')
-createApp(application).mount('#app')
+createApp(App).mount('#app')
