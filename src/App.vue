413508a5a0238ff94db4eaa504b4ddac08a858d6
vladimir360 <vladimir360@mail.ru>
2021-12-25 15:39:48 +0300

    add uncompletedTasks and completeTasks

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
     <div v-if="count() == 0">Вы великолепны!</div>
     <div v-else-if="count() == 1">Осталя последний рывок!</div>
     <div v-else>Осталось сделать задач: <span class="counter">{{count()}}</span></div>
+
+    
     <div class="list">
-      <div class="item" v-bind:class="{done: task.done}" v-for='task in tasks' :key="task">
+      <div class="item" v-bind:class="{done: task.done}" v-for='task in uncompletedTasks()' :key="task.text">
         <input type="checkbox" v-model="task.done">
         {{task.text}}<like/>
       </div>
     </div>
+
     <div class="form">
       <input v-model="textTask">
       <input type="submit" value="Добавить" @click="addTask">
@@ -16,9 +19,18 @@
     <transition name="bounce">
       <img src="rob.jpg" alt="#" v-show="count() == 0">
     </transition>
+
+    <div class="list">
+      <div class="item" v-bind:class="{done: task.done}" v-for='task in completeTasks()' :key="task.text">
+        <input type="checkbox" v-model="task.done">
+        {{task.text}}<like/>
+      </div>
+    </div>
+
 </template>
 
 <script>
+
 
 import like from './components/Like.vue'
 
@@ -42,6 +54,12 @@ export default {
         },
         count() {
             return this.tasks.filter(task => !task.done).length;
+        },
+        completeTasks() {
+          return this.tasks.filter(task => task.done);
+        },
+        uncompletedTasks() {
+          return this.tasks.filter(task => !task.done);
         }
     },
     components: {
